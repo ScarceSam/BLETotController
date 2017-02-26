07efdd6e0d1639813b9a3a2aa2173a92ee10c838
ScarceSam <tinker@sampearce.net>
2017-02-25 23:29:02 -0800

    cleaned up
    cleaned up comments, added a list of to do,  set outputs to match
    schematic, and added schematic to directory

--- BLETotController.ino
+++ BLETotController.ino
@@ -1,3 +1,21 @@
+/*  BLE controller for childs ride-on electric vehicle
+ *   relay outputs match BLEcontroller_v1.fzz schematic 
+ *   (save for; positive source needs to be added to code)
+ *  
+ *  notes
+ *  curently; 
+ *  test button 1 turns on/off ble control
+ *  test button 2 put vehicle in reverse and turns on throttle while pressed
+ *  test button 3 put vehicle in low speed and turns on throttle while pressed
+ *  test button 4 put vehicle in high speed and turns on throttle while pressed
+ * 
+ *  changes to be made;
+ *  test button 1 to stay as is
+ *  test button 2 to toggle between ble speed controll only (throttle controlled by child)
+ *  test button 3 to toggle between reverse, low, and high speeds
+ *  test button 4 = throttle when in ble full control mode
+ *  
+ */
 #include <BLEAttribute.h>
 #include <BLECentral.h>
 #include <BLECharacteristic.h>
@@ -11,43 +29,55 @@
 #include <CurieBLE.h>
 
 
-
-BLEPeripheral bleController; //set the name of the arduino for BLE
-BLEService controlService("9456767d-d15b-41a3-a697-4e2aa8f36ad1"); //create the BLE sevice
+//set the name of the arduino for BLE
+BLEPeripheral bleController;
+//create the BLE sevice with characteristics
+BLEService controlService("9456767d-d15b-41a3-a697-4e2aa8f36ad1"); 
 BLECharCharacteristic bleCharacteristic("7d99f31c-bc4f-4087-b0dc-f2ea2e7f0624", BLERead | BLEWrite);
 BLECharCharacteristic speedCharacteristic("ee80b338-0bda-4b03-b54f-80cfc3d55117", BLERead | BLEWrite);
 
-// define what ouput sets the relays to off and on
+/* define what ouput LOW/HIGH sets the relays to OFF and ON
+ *  using transistors to control the SainSmart Relay Module;
+ *  Arduino outputs need to be set to low to turn the relays off
+ *  and high to turn them on
+ */
 #define OFF LOW
 #define ON HIGH
 
-//set relay outputs
-const int rPositive = 0;
-const int rNegative = 1;
-const int rThrottle = 2;
-const int rBrake = 3;
-const int rRPCut = 4;
-const int rRNCut = 5;
-const int rLPCut = 6;
-const int rLNCut = 7;
-const int rRP = 8;
-const int rRN = 9;
-const int rLP = 10;
-const int rLN = 11;
- //set test button inputs
+//set relays
+const int rPositive = 6; // Positive lead from battery
+const int rNegative = 1; // Negative lead to battery
+const int rThrottle = 5; // Throttle relay
+const int rBrake = 4;    // Brake Relay
+
+// relays to cut motor leads over to BLE controller
+const int rRPCut = 3;    
+const int rRNCut = A1;
+const int rLPCut = 0;
+const int rLNCut = 12;
+
+// relays to control power to motor leads (rRP = relay Right Positive)
+const int rRP = 2;
+const int rRN = A0;
+const int rLP = 1;
+const int rLN = 13;
+
+// set test button inputs
 const int testButton1 = A2;
 const int testButton2 = A3;
 const int testButton3 = A4;
 const int testButton4 = A5;
-int test1State = 1;
+
+int blePower = 1;
 int test2State = 1;
 int test3State = 1;
 int test4State = 1;
-int test1PrevState = 1;
+int blePowerPrevState = 1;
 int test2PrevState = 1;
 int test3PrevState = 1;
 int test4PrevState = 1;
 int bleState = 0;
+
 const int debounceDelay = 50;
 unsigned long debounceTime1 = 0; 
 int buttonState = 0;
@@ -58,10 +88,11 @@ const long timer = 1000;
 long brakePoint = 2000;
 
 void BLEMode (int A); //swiching BLE and Child control; 0=disabled 1=enabled
-void motorRotatio (int B); //direction and speed of motor; 0=reverse, 1=lowspeed, 2=high speed
+void motorRotation (int B); //direction and speed of motor; 0=reverse, 1=lowspeed, 2=high speed
 
 void setup() {  
-  //Set all relays to outputs in the off state
+  
+  // Set all relays to outputs in the off state
   pinMode(rPositive,OUTPUT);
   digitalWrite(rPositive,OFF);
   pinMode(rNegative,OUTPUT);
@@ -86,7 +117,8 @@ void setup() {
   digitalWrite(rLP,OFF);
   pinMode(rLN,OUTPUT);
   digitalWrite(rLN,OFF);
-  
+
+  // set test button inputs
   pinMode(testButton1, INPUT_PULLUP);
   pinMode(testButton2, INPUT_PULLUP);
   pinMode(testButton3, INPUT_PULLUP);
@@ -107,91 +139,92 @@ void setup() {
 }
 
 void loop() {
-
+  // attempt ble connection
   BLECentral controlDevice = bleController.central();
 
-do{
-
-  int test1State = digitalRead(testButton1);
-  int test2State = digitalRead(testButton2);
-  int test3State = digitalRead(testButton3);
-  int test4State = digitalRead(testButton4);
-
-  if(test1State == 0 || bleCharacteristic.value() == 0){
-    test1State = 0;
-  }
-
-  if(test2State == 0 || speedCharacteristic.value() == 'R'){
-    test2State = 0;
-  }
-  
-  if(test3State == 0 || speedCharacteristic.value() == '1'){
-    test3State = 0;
-  }
-  
-  if(test4State == 0 || speedCharacteristic.value() == '2'){
-    test4State = 0;
-  }
-
-  //test turning BLE Control on and off
-  if (test1State != test1PrevState){
-    debounceTime1 = millis();
-  }
-
-
-  if ((millis() - debounceTime1) > debounceDelay) {
-    if (test1State != buttonState) {
-      buttonState = test1State;
+  // do-while loop alows code to run whether ble connection is made or not
+  do{
     
-      if (buttonState == LOW) {
-        bleState = !bleState;
-        BLEMode(bleState);
+    blePower = digitalRead(testButton1);
+    test2State = digitalRead(testButton2);
+    test3State = digitalRead(testButton3);
+    test4State = digitalRead(testButton4);
+
+    if(blePower == 0 || bleCharacteristic.value() == 0){
+      blePower = 0;
+    }
+
+    if(test2State == 0 || speedCharacteristic.value() == 'R'){
+      test2State = 0;
+    }
+  
+    if(test3State == 0 || speedCharacteristic.value() == '1'){
+      test3State = 0;
+    }
+  
+    if(test4State == 0 || speedCharacteristic.value() == '2'){
+      test4State = 0;
+    }
+
+    // **************************turn BLE Control on and off********************************************
+    if (blePower != blePowerPrevState){
+      debounceTime1 = millis();
+    }
+
+    if ((millis() - debounceTime1) > debounceDelay) {
+      if (blePower != buttonState) {
+        buttonState = blePower;
+    
+        if (buttonState == LOW) {
+          bleState = !bleState;
+          BLEMode(bleState);
+        }
       }
     }
-  }
-  test1PrevState = test1State;
+    blePowerPrevState = blePower;
+    // ***********************************************************************************************
 
-  //Test reverse
-  if((millis()-forwardTimer) > timer){
-    if(test2State == 0 && test2PrevState == 1){
-      motorRotatio(0);
-      test2PrevState = 0;
-    }else if(test2State == 1 && test2PrevState == 0){
-      digitalWrite(rThrottle, OFF);
-      reverseTimer = millis();
-      test2PrevState = 1;
+    //Test reverse
+    if((millis()-forwardTimer) > timer){
+      if(test2State == 0 && test2PrevState == 1){
+        motorRotation(0);
+        test2PrevState = 0;
+      }else if(test2State == 1 && test2PrevState == 0){
+        digitalWrite(rThrottle, OFF);
+        reverseTimer = millis();
+        test2PrevState = 1;
+      }
     }
-  }
 
-  //Test low speed
-  if((millis()- reverseTimer) > timer){
-    if(test3State == 0 && test3PrevState == 1){
-      motorRotatio(1);
-      test3PrevState = 0;
-    }else if(test3State == 1 && test3PrevState == 0){
-      digitalWrite(rThrottle, OFF);
-      forwardTimer = millis();
-      test3PrevState = 1;
+    //Test low speed
+    if((millis()- reverseTimer) > timer){
+      if(test3State == 0 && test3PrevState == 1){
+        motorRotation(1);
+        test3PrevState = 0;
+      }else if(test3State == 1 && test3PrevState == 0){
+        digitalWrite(rThrottle, OFF);
+        forwardTimer = millis();
+        test3PrevState = 1;
+      }
     }
-  }
 
-  //Test High speed 
-  if((millis()- reverseTimer) > timer){
-    if(test4State == 0 && test4PrevState == 1){
-      motorRotatio(2);
-      test4PrevState = 0;
-    }else if(test4State == 1 && test4PrevState == 0){
-      digitalWrite(rThrottle, OFF);
-      forwardTimer = millis();
-      test4PrevState = 1;
+    //Test High speed 
+    if((millis()- reverseTimer) > timer){
+      if(test4State == 0 && test4PrevState == 1){
+        motorRotation(2);
+        test4PrevState = 0;
+      }else if(test4State == 1 && test4PrevState == 0){
+        digitalWrite(rThrottle, OFF);
+        forwardTimer = millis();
+        test4PrevState = 1;
+      }
     }
-  }
-  if(test2State == 1 && test3State == 1 && test4State == 1){
-    if((millis() - reverseTimer) > brakePoint && (millis() - forwardTimer) > brakePoint){
-      digitalWrite(rBrake, OFF);
+    if(test2State == 1 && test3State == 1 && test4State == 1){
+      if((millis() - reverseTimer) > brakePoint && (millis() - forwardTimer) > brakePoint){
+        digitalWrite(rBrake, OFF);
+      }
     }
-  }
-}while (controlDevice.connected());
+  }while (controlDevice.connected());
 }
 
 void BLEMode(int A) {
@@ -200,8 +233,10 @@ void BLEMode(int A) {
     return; 
   }
 
-  //Turn on/off BLE control of the vehicle by Turning off the throttle, brake, and motor control relays (to prvent shorts or unwanted startup) 
-  //then turning on/off the Positive, negative, and motor cutout relays
+  /* Turn on/off BLE control of the vehicle by;
+  *  Turning control relays to normal state (to prvent shorts or unwanted startup) 
+  *  then turning on/off the Positive, negative, and motor cutout relays
+  */
   digitalWrite(rThrottle, OFF);
   digitalWrite(rBrake, OFF);
   digitalWrite(rRP, OFF);
@@ -227,7 +262,7 @@ void BLEMode(int A) {
   return;
 }
 
-void motorRotatio (int B){
+void motorRotation (int B){
 
   if (B < 0 || B > 2){
     return;
